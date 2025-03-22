141d741f4d4eb1913b97893596d26f4ce985b450
Buhle <mtshalizanobuhle7@gmail.com>
2025-03-22 22:32:08 +0200

    updated the search.css and the jsx file

--- recipe-app/src/components/Search.jsx
+++ recipe-app/src/components/Search.jsx
@@ -7,7 +7,12 @@ const SearchComponent = () => {
                 <input type="search" placeholder="e.g Salmon" id="input-search"/>
                 <button id="search-btn">+ Add ingredient</button>
             </div>
-            <div className="ingredients-container"></div>
+            <div className="ingredients-container">
+                <h1>Ingredients on hand:</h1>
+                <ul>
+                    <li>Chicken breasts</li>
+                </ul>
+            </div>
         </div>
     )
 }

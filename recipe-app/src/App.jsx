a3356a02f2c6aa6856b61c17c00e791716cae248
Buhle <mtshalizanobuhle7@gmail.com>
2025-03-23 10:12:31 +0200

    updated the components

--- recipe-app/src/App.jsx
+++ recipe-app/src/App.jsx
@@ -1,15 +1,46 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css';
 import Header from './components/Header';
-import SearchComponent from './components/Search';
 
 function App() {
+  //useState 
+  const [recipes, setRecipes] = useState([]);
+
+
+  //useEffect hook
+  useEffect(() => {
+    searchRecipes('American')
+  }, []);
+
+  //searchrecipe function
+  const searchRecipes = async (recipe) => {
+    const url = `https://themealdb.p.rapidapi.com/filter.php?a=${recipe}`;
+    const options = {
+      method: 'GET',
+      headers: {
+        'x-rapidapi-key': 'eea6a34f4dmsh296963a5654d19ep13d93ajsn94fa453c501f',
+        'x-rapidapi-host': 'themealdb.p.rapidapi.com'
+      }
+    }
+      const response = await fetch(url, options);
+      const data = await response.json();
+      console.log(data.meals);
+  }
+
 
 
   return (
     <div className='app-container'>
       <Header />
-      <SearchComponent />
+
+      {/* search container */}
+      <div className="search-div">
+      <input type="search" placeholder="e.g Salmon" id="input-search" />
+      <button id="search-btn" >+ Add ingredient</button>
+      </div>
+      {/* search container */}
+        
+      
     </div>
   )
 }
